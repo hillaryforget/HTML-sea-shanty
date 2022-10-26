c25e1c9975185cc1bc96303a309ffc4c51daf6c0
Hillary Forget <hillaryforget@outlook.com>
2022-10-26 10:10:14 -0400

    sea shanty

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express();
 app.use(express.static(__dirname + '/public'));
 app.set("view engine", 'ejs');
 
-app.get('/', (req, res) => { 
-  res.render("htmlSeaShanty")
-})
+app.get('/', (req, res) => {
+  res.render("htmlSeaShanty");
+});
 
-app.listen(3000, () => { 
-  console.log('Server listening on port 3000, ready for an old-fashioned sea shanty.')
-})
+app.listen(3000, () => {
+  console.log('Server listening on port 3000, ready for an old-fashioned sea shanty.');
+});
